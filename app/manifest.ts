f9ca98dae022c297a6cf7f01826ad03101943733
mactar27 <mactar27@example.com>
2026-05-13 16:00:54 +0000

    Optimize logo size and update manifest for better shortcut support

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,11 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/ndugu.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any maskable',
       },
       {
         src: '/ndugu.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any maskable',
       },
     ],
   }
